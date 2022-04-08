cc642bbc052b99ad2452b2fa7d0307c994b7afab
LeonidRos <102150359+LeonidRos@users.noreply.github.com>
2022-04-08 03:21:33 +0300

    is done without last task

--- scripts.js
+++ scripts.js
@@ -59,3 +59,194 @@ function showArray(array){
 
 showArray(arr);
 
+
+console.warn('Задание 5------------------');
+
+function isEven(num){
+
+    num /= 2;
+    let int = parseInt(num);
+
+    if(+int === +num){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isEven(4040141));
+
+
+console.warn('Задание 6------------------');
+
+function isEvenArray(arr){
+
+    let arrEven = [];
+    for(let i = 0; i < arr.length; i++){
+
+        if(isEven(arr[i])){
+            arrEven.push(arr[i]);
+        }
+    }
+
+    return arrEven;
+}
+
+console.log(isEvenArray([1, 2, 6, 99, 88, 4, 5, 9]));
+
+
+console.warn('Задание 7------------------');
+
+function drawPyromid(rows){
+
+    for(let i = 0; i <= rows; i++){
+
+        let columns = '';
+
+        for(let j = 0; j < i; j++){
+            
+            if(arguments[1] == undefined)columns += i;    
+            else if(arguments[1] != ' ')columns += arguments[1];;
+        }
+
+        if(arguments[1] == ' '){
+
+            console.error('Error: don\'t use space');
+            break;
+        }
+        console.log(columns);
+    }
+}
+
+drawPyromid(9);
+drawPyromid(9, ' ');
+drawPyromid(9, '@');
+
+
+console.warn('Задание 8------------------');
+
+//Первая часть была в прошлой дамашке, поэтому написал только вторую часть.
+
+function drawTriangleReverse(){
+
+
+    let rows = prompt('Задание 10. Введите кол-во рядов треугольника...');
+
+    let triSpace = 0,
+        triChar = 0;
+
+    for(let i = rows; i > 0; i--){
+
+        triChar = i * 2 - 1;
+        triSpace = rows - i;
+        console.log(' '.repeat(triSpace), '*'.repeat(triChar));
+    }
+}
+
+drawTriangleReverse();
+
+
+console.warn('Задание 9------------------');
+
+function pushArrayFibb(){
+
+    let f0 = 0,
+        f1 = 1,
+        f2 = 0,
+        arrFib = [];
+
+    arrFib.push(0, 1);
+
+    while(arrFib[arrFib.length - 1] <= 1000){
+
+        f2 = f0 + f1;
+        arrFib.push(f2);
+
+        f0 = f1;
+        f1 = f2;
+    }
+    return arrFib;
+}
+
+console.log(pushArrayFibb());  
+
+
+console.warn('Задание 10------------------');
+
+function lessThanNine(int){
+
+    int += '';
+    let less9 = 0;
+
+    for(let i = 0; i < int.length; i++){
+
+        less9 += +int[i];  
+    }
+
+    if(less9 > 9)lessThanNine(less9);
+    else return less9;
+}
+console.log(lessThanNine(44554));
+console.log(`почему-то возвращается undefined если 
+передавать в функцию 3-х и более значное число... 
+я тыркался, тыркался... но так и не разобрался`);
+
+console.warn('Задание 11------------------');
+
+function testRecursion(array){
+
+    if(arguments[1] == undefined)arguments[1] = 0;
+
+    if(array.length > arguments[1]){
+
+        console.log(array[arguments[1]]);
+        testRecursion(array, arguments[1] + 1);
+    }
+}
+
+testRecursion([1, 5, 44, 'oh sh*t', true, 'this is a ghoust', 9]);
+
+console.warn('Задание 12------------------');
+
+function getName(){
+
+    let fname = prompt('Задание 12. Введите ваше имя'),
+        sname = prompt('Введите вашу фамилию'),
+        fathname = prompt('Введите ваше отчество'),
+        group = prompt('Введите номер вашей группы'),
+
+        row1 = 'Домашняя работа: «Функции»',
+        row2 = 'Выполнил: студент гр. ' + group,
+        row3 = '  ' + sname + fname + fathname,
+        maxN = -Infinity,
+        arr = [];
+
+    arr.push(row1.length, row2.length, row3.length);
+
+
+    for(let i = 0; i < 3; i++){
+
+        if(maxN < arr[i])maxN = arr[i];
+    }
+
+    console.log('*'.repeat(maxN + 4));
+    console.log(`* Домашняя работа: «Функции» ${' '.repeat(maxN - row1.length)}*`);
+    console.log(`* Выполнил: студент гр. ${group} ${' '.repeat(maxN - row2.length)}*`);
+    console.log(`* ${sname} ${fname} ${fathname} ${' '.repeat(maxN - row3.length)}*`);
+    console.log('*'.repeat(maxN + 4));  
+}
+
+getName();
+
+// Курс «JS».
+// 13. Напишите ф-цию, которая должна проверить правильность ввода адреса
+// эл. почты, неиспользуя регулярные выражения. Почта верна при условии:
+// a. весь адрес не должен содержать русские буквы и спецсимволы, кроме
+// одной «собачки», знака подчеркивания, дефиса и точки, причем они не могут
+// быть первыми и последними в адресе, и идти подряд, например: «..», «@.»,
+// «.@» или «@@», «_@», «@-», «--» и т.п.
+// b. имя эл. почты (до знака @) должно быть длиной более 2 символов, причем
+// имя может содержать только буквы, цифры, но не быть первыми и
+// единственными в имени, и точку;
+// c. после последней точки и после @, домен верхнего уровня (ru, by, com и
+// т.п.) не может быть длиной менее 2 и более 11 символов.
