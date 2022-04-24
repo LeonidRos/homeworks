4ec79bfdc27678f7562c3aecc550fb0936e59d78
LeonidRos <102150359+LeonidRos@users.noreply.github.com>
2022-04-25 00:01:21 +0300

    done!

--- scripts.js
+++ scripts.js
@@ -1,77 +1,167 @@
+let html = document.querySelector('html');
+html.setAttribute('leng', 'ru');
 
+let headTitile = document.createElement('title');
+headTitile.innerHTML = 'Choose Your Option';
+document.head.append(headTitile);
 
-const Car = function(){ 
+let headMeta = document.createElement('meta');
+headMeta.setAttribute('charset', 'utf-8');
+document.head.append(headMeta);
 
-    let typeFuel, fuel, weight, engVol, fuelcoff;
+let linkHead = document.createElement('link');
+linkHead.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Arvo&family=Montserrat&family=Open+Sans&display=swap');
+linkHead.setAttribute('rel', 'stylesheet');
+document.head.append(linkHead);
 
-    this.setFuelType = function(t = 'Дизель'){
+let mainElem = document.createElement('div');
+mainElem.classList.add('main');
+document.body.append(mainElem);
+mainElem.style.display = 'flex';
+mainElem.style.justifyContent = 'center';
+mainElem.style.flexDirection = 'column';
+mainElem.style.alignItems = 'center';
+mainElem.style.fontFamily = `Arvo, serif`;
 
-        if(!isNaN(t)){
-            console.log('Введите тип топлива. "Бензин", "Дизель", "Пропан", "Метан"');
-            return;
-        }
-        typeFuel = t;
-        fuelcoff = this.getFuelCoff(t);
-    }
+let h2Elem = document.createElement('h2');
+h2Elem.classList.add('header2');
+h2Elem.innerHTML = 'Choose Your Option';
+h2Elem.style.fontSize = '36px';
+mainElem.append(h2Elem);
 
-    this.setFuel = function(f = 10){
+let spanElem1 = document.createElement('span');
+spanElem1.classList.add('monss');
+spanElem1.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing ';
+spanElem1.style.fontSize = '16px';
+spanElem1.style.fontFamily = 'Open Sans, sans-serif';
+spanElem1.style.color = '#9FA3A7';
+mainElem.append(spanElem1);
 
-        if(isNaN(f)){
-            console.log('Введите кол-во топлива в литрах.');
-            return;
-        }
-        fuel = +f;
-    }
+let divContent = document.createElement('div');
+divContent.classList.add('main__content');
+divContent.style.margin = '30px 0';
+divContent.style.display = 'flex';
+mainElem.append(divContent);
 
-    this.setWeight = function(w = 1350){
+let divLeftElem = document.createElement('div');
+divLeftElem.classList.add('left_side');
+divLeftElem.style.height = '480px';
+divLeftElem.style.width = '400px';
+divLeftElem.style.display = 'flex';
+divLeftElem.style.justifyContent = 'space-evenly';
+divLeftElem.style.flexDirection = 'column';
+divLeftElem.style.alignItems = 'center';
+divLeftElem.style.textAlign = 'center';
+divLeftElem.style.borderLeft = '1px solid #ededed';
+divLeftElem.style.borderTop = '1px solid #ededed';
+divLeftElem.style.borderBottom = '1px solid #ededed';
+divLeftElem.style.borderRadius = '8px 0 0 8px';
+divLeftElem.style.textAlign = 'center';
+divLeftElem.style.textAlign = 'center';
+divLeftElem.style.padding = '50px 50px';
+divContent.append(divLeftElem);
 
-        if(isNaN(w)){
-            console.log('Введите вес авто в кг.');
-            return;
-        }
-        weight = +w;
-    }
+let spanElem2 = document.createElement('span');
+spanElem2.classList.add('opens');
+spanElem2.innerHTML = 'freelanser';
+spanElem2.style.fontSize = '16px';
+spanElem2.style.fontFamily = 'Montserrat, sans-serif';
+spanElem2.style.color = '#9FA3A7';
+spanElem2.style.fontWeight = 'bold';
+spanElem2.style.textTransform = 'uppercase';
+spanElem2.style.letterSpacing = '2.4px';
+divLeftElem.append(spanElem2);
 
-    this.setEngeVol = function(e = 2.0){
+let h3Elem = document.createElement('h3');
+h3Elem.classList.add('header3');
+h3Elem.innerHTML = 'Initially designed to ';
+h3Elem.style.fontSize = '36px';
+h3Elem.style.padding = '0 60px';
+divLeftElem.append(h3Elem);
 
-        if(isNaN(e)){
-            console.log('Введите объем двигателя в литрах.');
-            return;
-        }
-        engVol = +e;
-    }
-    
-    this.getResult = function(){
+let spanElem3 = document.createElement('span');
+spanElem3.classList.add('opens');
+spanElem3.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing ';
+spanElem3.style.fontSize = '16px';
+spanElem3.style.fontFamily = 'Open Sans, sans-serif';
+spanElem3.style.color = '#9FA3A7';
+spanElem3.style.padding = '0 60px';
+spanElem3.style.lineHeight = '26px';
+divLeftElem.append(spanElem3);
 
-        
-        let result = fuel * (fuelcoff) / (weight / 1000) / (engVol / 40);
-  
-        console.log(`
-       
-        Объем двигателя: ${engVol}л.
-        Вес авто: ${weight}кг.
-        Тип топлива: ${typeFuel}.
-        Кол-во топлива: ${fuel}л.
-        Вы сможете проехать около: ${result}км.
+let btnElem1 = document.createElement('button');
+btnElem1.classList.add('btn1');
+btnElem1.innerHTML = 'start here';
+btnElem1.style.border = '3px solid #FFC80A';
+btnElem1.style.borderRadius = '30px';
+btnElem1.style.padding = '15px 30px';
+btnElem1.style.backgroundColor = 'white';
+btnElem1.style.textTransform = 'uppercase';
+btnElem1.style.fontFamily = 'Montserrat, sans-serif';
+btnElem1.style.letterSpacing = '2.4px';
+btnElem1.style.fontWeight = 'bold';
+btnElem1.style.cursor = 'pointer';
+divLeftElem.append(btnElem1);
 
-        `);
-    }
-    
-    this.getFuelCoff = function(typeFuel){
+let divRightElem = document.createElement('div');
+divRightElem.classList.add('left_side');
+divRightElem.style.height = '480px';
+divRightElem.style.width = '400px';
+divRightElem.style.display = 'flex';
+divRightElem.style.justifyContent = 'space-evenly';
+divRightElem.style.flexDirection = 'column';
+divRightElem.style.alignItems = 'center';
+divRightElem.style.textAlign = 'center';
+divRightElem.style.borderRight = '1px solid #ededed';
+divRightElem.style.borderTop = '1px solid #ededed';
+divRightElem.style.borderBottom = '1px solid #ededed';
+divRightElem.style.borderRadius = '0 8px 8px 0';
+divRightElem.style.textAlign = 'center';
+divRightElem.style.textAlign = 'center';
+divRightElem.style.padding = '50px 50px';
+divRightElem.style.backgroundColor = '#8F75BE';
+divContent.append(divRightElem);
 
-        if(typeFuel == 'Бензин')return fuelcoff = 0.7;
-        else if(typeFuel == 'Дизель')return fuelcoff = 1.2;
-        else if(typeFuel == 'Пропан')return fuelcoff = 1.4;
-        else if(typeFuel == 'Метан')return fuelcoff = 1.5;
-    }
+let spanElem8 = document.createElement('span');
+spanElem8.classList.add('opens');
+spanElem8.innerHTML = 'studio';
+spanElem8.style.fontSize = '16px';
+spanElem8.style.fontFamily = 'Montserrat, sans-serif';
+spanElem8.style.color = '#FFC80A';
+spanElem8.style.fontWeight = 'bold';
+spanElem8.style.textTransform = 'uppercase';
+spanElem8.style.letterSpacing = '2.4px';
+divRightElem.append(spanElem8);
 
-};
+let h3Elem1 = document.createElement('h3');
+h3Elem1.classList.add('header3');
+h3Elem1.innerHTML = 'Initially designed to ';
+h3Elem1.style.fontSize = '36px';
+h3Elem1.style.padding = '0 60px';
+h3Elem1.style.color = 'white';
+divRightElem.append(h3Elem1);
 
-let car = new Car();
-car.setEngeVol();
-car.setFuel();
-car.setWeight();
-car.setFuelType();
-car.getResult();
+let spanElem33 = document.createElement('span');
+spanElem33.classList.add('opens');
+spanElem33.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing ';
+spanElem33.style.fontSize = '16px';
+spanElem33.style.fontFamily = 'Open Sans, sans-serif';
+spanElem33.style.color = '#fff';
+spanElem33.style.padding = '0 60px';
+spanElem33.style.lineHeight = '26px';
+divRightElem.append(spanElem33);
 
-//Формула не работает. Слишком много переменных сложно подобрать правильную зависимость.
+let btnElem2 = document.createElement('button');
+btnElem2.classList.add('btn1');
+btnElem2.innerHTML = 'start here';
+btnElem2.style.border = '3px solid #FFC80A';
+btnElem2.style.borderRadius = '30px';
+btnElem2.style.padding = '15px 30px';
+btnElem2.style.backgroundColor = 'transparent';
+btnElem2.style.textTransform = 'uppercase';
+btnElem2.style.fontFamily = 'Montserrat, sans-serif';
+btnElem2.style.letterSpacing = '2.4px';
+btnElem2.style.fontWeight = 'bold';
+btnElem2.style.color = 'white';
+btnElem2.style.cursor = 'pointer';
+divRightElem.append(btnElem2);
